554be2c83fd2fa15c5a1718b06511fbee531b434
ikachang <ikanikan1729@gmail.com>
2023-03-21 00:14:28 +0900

    tutorial chapter 3

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,69 +1,36 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import { remark } from 'remark'
-import html from 'remark-html'
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
 
-const postsDirectory = path.join(process.cwd(), 'posts')
-
-export function getSortedPostsData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(fileName => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '')
-
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName)
-    const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents)
-
-    // Combine the data with the id
-    return {
-      id,
-      ...(matterResult.data as { date: string; title: string })
-    }
-  })
-  // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1
-    } else {
-      return -1
-    }
-  })
-}
-
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory)
-  return fileNames.map(fileName => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, '')
-      }
-    }
-  })
-}
-
-export async function getPostData(id: string) {
-  const fullPath = path.join(postsDirectory, `${id}.md`)
-  const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents)
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content)
-  const contentHtml = processedContent.toString()
-
-  // Combine the data with the id and contentHtml
-  return {
-    id,
-    contentHtml,
-    ...(matterResult.data as { date: string; title: string })
-  }
-}
+const postsDirectory = path.join(process.cwd(), 'posts');
+export type PostData = {
+	id: string;
+	date: string;
+	title: string;
+};
+export const getSortedPostsData = (): PostData[] => {
+	// /postsディレクトリ内のファイル名を得る
+	const fileNames = fs.readdirSync(postsDirectory);
+	const allPostsData = fileNames.map((fileName) => {
+		// 拡張子".md"をファイル名から除く
+		const id = fileName.replace(/\.md$/, '');
+		// マークダウンファイルを文字列として読み込む
+		const fullPath = path.join(postsDirectory, fileName);
+		const fileContents = fs.readFileSync(fullPath, 'utf8');
+		// gray-matterでpostのメタデータ部分を解析する
+		const matterResult = matter(fileContents);
+		// 取り出したデータにidを組み合わせる
+		return {
+			id,
+			...matterResult.data as {date: string; title: string},
+		};
+	});
+	// postを日付順に並べ替える
+	return allPostsData.sort((a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+	});
+};
